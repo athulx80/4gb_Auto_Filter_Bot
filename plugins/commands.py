b1950bea547c42531e17469a55b5418766317c06
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-08 13:48:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,7 +36,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
             #photo=random.choice(PICS),
-            caption=script.KFC.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.KFC.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             #reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -82,7 +82,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
             #photo=random.choice(PICS),
-            caption=script.KFC.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.KFC.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             #reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
