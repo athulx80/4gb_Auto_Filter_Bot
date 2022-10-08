d5bd93baea3cec93260fe0238224fa4c7794e71e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-08 11:59:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/DHANANJAY4532/4gb_Auto_Filter_Bot.git /4gb_Auto_Filter_Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /4gb_Auto_Filter_Bot
 fi
-cd /EvaMaria
+cd /4gb_Auto_Filter_Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
