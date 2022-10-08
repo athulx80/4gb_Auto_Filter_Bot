83754f149243f74ca2ad441dc9210cae7afa27d2
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-08 14:07:08 +0530

    Update admin_start.py

--- plugins/admin_start.py
+++ plugins/admin_start.py
@@ -22,7 +22,7 @@ async def admin_start(client, message):
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
+        await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
